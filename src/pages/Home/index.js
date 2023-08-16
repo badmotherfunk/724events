84aa7524e94631dee8437c467d9f9ee673170fa4
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2023-08-16 18:08:30 +0200

    Ajout de la modal pour la carte du dernier évènement affiché dans le footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import Logo from "../../components/Logo";
 import Icon from "../../components/Icon";
 import Form from "../../containers/Form";
 import Modal from "../../containers/Modal";
+import ModalEvent from "../../containers/ModalEvent";
 import { useData } from "../../contexts/DataContext";
 
 
@@ -125,15 +126,19 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
-          data-testid="last-event-card"
-          imageSrc={last?.cover}
-          imageAlt={last?.description}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label={last?.type}
-        />
+        {last &&
+          <Modal key={last.id} Content={<ModalEvent event={last} />}>
+            {({ setIsOpened }) => (
+              <EventCard
+                onClick={() => setIsOpened(true)}
+                imageSrc={last.cover}
+                title={last.title}
+                date={new Date(last.date)}
+                label={last.type}
+              />
+            )}
+          </Modal>
+        }
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
